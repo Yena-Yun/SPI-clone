2cc264b7574c96c556789e370fb199a5cc7a5850
Yena-Yun <cheryleduart@gmail.com>
2022-12-29 13:19:47 +0900

    feat: Header CSS 구현

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -1,121 +1,150 @@
-import classNames from 'classnames';
+import classnames from 'classnames';
 import styles from './index.module.scss';
 
 export const Header = () => {
   return (
     <header className={styles.header}>
-      <div className={styles.logoWrap}>
-        <a href='https://seoulpi.co.kr/'>
-          <img
-            src='https://seoulpi.co.kr/wp-content/uploads/2021/02/SPI-whit-logo-1.svg'
-            alt='SPI white logo'
-          />
-        </a>
-      </div>
-      <div className={styles.contentWrap}>
-        <div className={styles.subscribeButtonWrap}>
-          <a
-            href='https://seoulpi.co.kr/membership/'
-            className={styles.subscribeButton}
-          >
-            MEMBERSHIP
-          </a>
-        </div>
-        <div className={styles.actionWrap}>
-          <ul className={styles.socialMediaList}>
-            <li className={styles.socialMediaListItem}>
-              <a
-                href='https://www.instagram.com/seoulpropertyinsight/'
-                className={styles.icon}
-                title='인스타그램 팔로 잉'
-                target='_blank'
-                rel='noreferrer'
-              >
-                {/* before */}
-                <span className={styles.socialMediaName} aria-hidden>
-                  팔로우
-                </span>
-              </a>
-            </li>
-            <li className={styles.socialMediaListItem}>
-              <a
-                href='https://www.linkedin.com/in/seoul-property-insight-spi-91686b190/'
-                className={styles.icon}
-                title='LinkedIn 팔로 잉'
-                target='_blank'
-                rel='noreferrer'
-              >
-                {/* before */}
-                <span className={styles.socialMediaName} aria-hidden>
-                  팔로우
-                </span>
-              </a>
-            </li>
-            <li className={styles.socialMediaListItem}>
-              <a
-                href='https://www.facebook.com/profile.php?id=100067101784808'
-                className={styles.icon}
-                title='Facebook 팔로 잉'
-                target='_blank'
-                rel='noreferrer'
-              >
-                {/* before */}
-                <span className={styles.socialMediaName} aria-hidden>
-                  팔로우
-                </span>
-              </a>
-            </li>
-            <li className={styles.socialMediaListItem}>
-              <a
-                href='https://twitter.com/SPI45323666'
-                className={styles.icon}
-                title='Twitter 팔로 잉'
-                target='_blank'
-                rel='noreferrer'
-              >
-                {/* before */}
-                <span className={styles.socialMediaName} aria-hidden>
-                  팔로우
-                </span>
-              </a>
-            </li>
-            <li className={styles.socialMediaListItem}>
-              <a
-                href='https://www.youtube.com/channel/UCA_IYvxjOsjGJ7-CnfsIScA'
-                className={styles.icon}
-                title='Youtube 팔로 잉'
-                target='_blank'
-                rel='noreferrer'
-              >
-                {/* before */}
-                <span className={styles.socialMediaName} aria-hidden>
-                  팔로우
-                </span>
-              </a>
-            </li>
-          </ul>
-          <div className={styles.menuWrap}>
-            <nav className={styles.menuNav}>
-              <ul className={styles.menuList}>
-                <li className={styles.menuListItem}>
-                  <a href='https://seoulpi.co.kr/login/'>LOGIN</a>
+      <div className={styles.headerOuter}>
+        <div className={styles.headerInner}>
+          <div className={styles.logoWrap}>
+            <a href='https://seoulpi.co.kr/'>
+              <img
+                src='https://seoulpi.co.kr/wp-content/uploads/2021/02/SPI-whit-logo-1.svg'
+                alt='SPI white logo'
+              />
+            </a>
+          </div>
+          <div className={styles.contentWrap}>
+            <div className={styles.actionWrap}>
+              <ul className={styles.socialMediaList}>
+                <li
+                  className={classnames(
+                    styles.socialMediaListItem,
+                    styles.socialInstagram
+                  )}
+                >
+                  <a
+                    href='https://www.instagram.com/seoulpropertyinsight/'
+                    className={styles.icon}
+                    title='인스타그램 팔로 잉'
+                    target='_blank'
+                    rel='noreferrer'
+                  >
+                    {/* before */}
+                    <span className={styles.socialMediaName} aria-hidden>
+                      팔로우
+                    </span>
+                  </a>
                 </li>
-                <li className={styles.menuListItem}>
-                  <a href='https://seoulpi.co.kr/spireg/'>REGISTER</a>
+                <li
+                  className={classnames(
+                    styles.socialMediaListItem,
+                    styles.socialLinkedIn
+                  )}
+                >
+                  <a
+                    href='https://www.linkedin.com/in/seoul-property-insight-spi-91686b190/'
+                    className={styles.icon}
+                    title='LinkedIn 팔로 잉'
+                    target='_blank'
+                    rel='noreferrer'
+                  >
+                    {/* before */}
+                    <span className={styles.socialMediaName} aria-hidden>
+                      팔로우
+                    </span>
+                  </a>
+                </li>
+                <li
+                  className={classnames(
+                    styles.socialMediaListItem,
+                    styles.socialFacebook
+                  )}
+                >
+                  <a
+                    href='https://www.facebook.com/profile.php?id=100067101784808'
+                    className={styles.icon}
+                    title='Facebook 팔로 잉'
+                    target='_blank'
+                    rel='noreferrer'
+                  >
+                    {/* before */}
+                    <span className={styles.socialMediaName} aria-hidden>
+                      팔로우
+                    </span>
+                  </a>
+                </li>
+                <li
+                  className={classnames(
+                    styles.socialMediaListItem,
+                    styles.socialTwitter
+                  )}
+                >
+                  <a
+                    href='https://twitter.com/SPI45323666'
+                    className={styles.icon}
+                    title='Twitter 팔로 잉'
+                    target='_blank'
+                    rel='noreferrer'
+                  >
+                    {/* before */}
+                    <span className={styles.socialMediaName} aria-hidden>
+                      팔로우
+                    </span>
+                  </a>
+                </li>
+                <li
+                  className={classnames(
+                    styles.socialMediaListItem,
+                    styles.socialYoutube
+                  )}
+                >
+                  <a
+                    href='https://www.youtube.com/channel/UCA_IYvxjOsjGJ7-CnfsIScA'
+                    className={styles.icon}
+                    title='Youtube 팔로 잉'
+                    target='_blank'
+                    rel='noreferrer'
+                  >
+                    {/* before */}
+                    <span className={styles.socialMediaName} aria-hidden>
+                      팔로우
+                    </span>
+                  </a>
                 </li>
               </ul>
-            </nav>
-          </div>
-          <div className={styles.languageSwitcher}>
-            <p>
-              <strong>KOR</strong>
-              <strong>|</strong>
-              <a href='https://en.seoulpi.co.kr'>ENG</a>
-            </p>
-            {/* <p>
+              <div className={styles.menuWrap}>
+                <nav className={styles.menuNav}>
+                  <ul className={styles.menuList}>
+                    <li className={styles.menuListItem}>
+                      <a href='https://seoulpi.co.kr/login/'>LOGIN</a>
+                    </li>
+                    <li className={styles.menuListItem}>
+                      <a href='https://seoulpi.co.kr/spireg/'>REGISTER</a>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
+              <div className={styles.languageSwitcher}>
+                <p>
+                  <strong>KOR</strong>
+                  <strong> | </strong>
+                  <a href='https://en.seoulpi.co.kr'>ENG</a>
+                </p>
+                {/* <p>
               <a href='https://seoulpi.co.kr'>KOR</a>
               <strong>| ENG</strong>
             </p> */}
+              </div>
+            </div>
+            <div className={styles.subscribeButtonWrap}>
+              <a
+                href='https://seoulpi.co.kr/membership/'
+                className={styles.subscribeButton}
+              >
+                MEMBERSHIP
+              </a>
+            </div>
           </div>
         </div>
       </div>
